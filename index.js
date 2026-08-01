2d237ad216f2fbee091e9141da74b7ed6fd5300d
Bishal Chalise <bishalise@gmail.com>
2026-08-01 14:53:41 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express')
-const cors = require('cors')
 var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
+app.use(express.static('dist'))
+
 
 morgan.token("body", (req) => {
     return JSON.stringify(req.body);
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-
 
 let persons = [
     {
