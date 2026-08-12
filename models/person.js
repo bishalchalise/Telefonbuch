e6f7599a7043e795cf2c0a6a16b013549d4f1664
Bishal Chalise <bishalise@gmail.com>
2026-08-12 20:19:05 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -16,7 +16,15 @@ const contactSchema = new mongoose.Schema({
         required: true,
         minLength: 3,
     },
-    number: String,
+    number: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                return /^\d{2,}-\d{7,}$/.test(value)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        }
+    }
 })
 
 
